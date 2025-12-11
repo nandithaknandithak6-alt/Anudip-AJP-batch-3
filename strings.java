52693f44433bb19cbd1f00f699ee7b8397a694f2
nandithaknandithak6-alt <nandithaknandithak6@gmail.com>
2025-12-11 19:46:45 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,14 @@
+public class strings {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = new String("Java");
+        if (s1 == s2)
+        {
+            System.out.println("equal");
+        }
+        else
+        {
+            System.out.println("Not equal");
+        }
+    }
+}
